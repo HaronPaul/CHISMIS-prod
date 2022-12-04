577fa4af3ec5b85abae152bd5c107524f4166d2f
HaronPaul <hplorente@up.edu.ph>
2022-12-04 21:32:09 +0800

    changed allowed origin ur

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,3 +1,3 @@
-const allowedOrigins = ['http://localhost:3000', 'https://chismismvc-app.onrender.com']
+const allowedOrigins = ['http://localhost:3000', 'https://chismismvc.onrender.com/']
 
 module.exports = allowedOrigins
